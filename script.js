edba2d8874463721666655af9430b4af9e9111f5
klyzecarize <71118473+klyzecarize@users.noreply.github.com>
2025-08-10 00:22:34 +0800

    test form submit on js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("it works")
+const taskForm = document.getElementById("taskForm");
+const sample = document.getElementById("sample")
+
+taskForm.addEventListener('submit', (event) => {
+    // prevents page to refresh when you click submit
+    event.preventDefault();
+
+    // test
+    console.log('Clickahh')
+    sample.innerHTML = "asdas";
+});
