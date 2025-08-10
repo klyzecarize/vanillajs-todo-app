a11165a5f12f2f81e8b3d50c98fd428e54f2dac9
klyzecarize <71118473+klyzecarize@users.noreply.github.com>
2025-08-10 21:56:47 +0800

    added function for adding new row when submit button is clicked

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const taskForm = document.getElementById("taskForm");
-const sample = document.getElementById("sample")
+const tbodyTag = document.getElementById("tbodyTag");
+const emptyEl = document.getElementById("empty");
+let sampleInt = 0;
 
 taskForm.addEventListener('submit', (event) => {
     // prevents page to refresh when you click submit
@@ -7,13 +9,41 @@ taskForm.addEventListener('submit', (event) => {
 
     // test
     console.log('Clickahh')
-    sample.innerHTML = "asdas";
+
+    addTableRow();
 });
 
 function deleteRow(childEl) {
+    removeRow(childEl);
+}
+
+function addTableRow () {
+    emptyEl != null && removeRow(emptyEl);
+    
+    const rowHtml = `
+        <tr>
+            <td>
+                <input type="checkbox" name="${sampleInt}" id="${sampleInt}">
+            </td>
+            <td>${sampleInt}</td>
+            <td>sample${sampleInt}</td>
+            <td>
+                <button type="button" class="btn btn-danger" onclick="deleteRow(this)">Delete</button>
+            </td>
+        </tr>
+    `;
+
+    // {html tag}.inserAdjacentHTML(position, html) - inserts HTML code into a specified position
+    tbodyTag.insertAdjacentHTML('beforeend', rowHtml);
+
+    sampleInt++;
+}
+
+function removeRow (childEl) {
     // get the closest tr element of childEl
     let getTr = childEl.closest('tr');
 
     // Remove the row element
     getTr.remove();
+
 }
